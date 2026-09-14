527a6d9f3d1bd1c69f5239fa877cd43d16a47249
Lin Xianglin <1021538027@qq.com>
2026-09-14 13:20:05 +0800

    drm/panel: clear inversion latch after normal mode

--- drivers/gpu/drm/panel/panel-samsung-amb655uv01.c
+++ drivers/gpu/drm/panel/panel-samsung-amb655uv01.c
@@ -159,6 +159,10 @@ static int samsung_amb655uv01_on(struct samsung_amb655uv01 *ctx)
 	mipi_dsi_dcs_set_display_on_multi(&dsi_ctx);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, MIPI_DCS_ENTER_NORMAL_MODE);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0x9f, 0xa5, 0xa5);
+	/* Some AMB655 revisions apply the inversion latch only after normal
+	 * mode is entered.  Repeat the standard DCS command with the vendor
+	 * page key closed so warm boots cannot retain the negative-color state. */
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, MIPI_DCS_EXIT_INVERT_MODE);
 
 	return dsi_ctx.accum_err;
 }
